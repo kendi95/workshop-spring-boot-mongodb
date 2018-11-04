bacc0f9588e8cda55bab6128023ed76bacb43dba
Alisson Kohatsu <kohatsukendi@gmail.com>
2018-11-03 22:28:52 -0300

    Projeto criado

---
+++ src/main/java/com/kohatsu/workshopmongodb/WorkshopMongodbApplication.java
@@ -0,0 +1,12 @@
+package com.kohatsu.workshopmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopMongodbApplication.class, args);
+	}
+}
