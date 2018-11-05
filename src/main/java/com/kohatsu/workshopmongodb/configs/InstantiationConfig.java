c608a16acf5812b17611e4f8d73d8cbd673d79ec
Alisson Kohatsu <kohatsukendi@gmail.com>
2018-11-05 12:34:09 -0200

    Acrescentando comentários aos posts

--- src/main/java/com/kohatsu/workshopmongodb/configs/InstantiationConfig.java
+++ src/main/java/com/kohatsu/workshopmongodb/configs/InstantiationConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.kohatsu.workshopmongodb.collections.Post;
 import com.kohatsu.workshopmongodb.collections.User;
 import com.kohatsu.workshopmongodb.dto.AuthorDTO;
+import com.kohatsu.workshopmongodb.dto.CommentDTO;
 import com.kohatsu.workshopmongodb.repositories.PostRepository;
 import com.kohatsu.workshopmongodb.repositories.UserRepository;
 
@@ -39,6 +40,13 @@ public class InstantiationConfig implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO comment1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO comment2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO comment3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(comment1, comment2));
+		post2.getComments().addAll(Arrays.asList(comment3));
 		
 		postRepo.saveAll(Arrays.asList(post1, post2));
 		
